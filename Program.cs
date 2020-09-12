f34c4f1c2c1e6b2c5d40a256fd38ed49a5c515ba
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-11 23:34:46 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace olaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeUsuario; 
+            Console.Write("Digite seu nome: ");
+            nomeUsuario = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Olá "+nomeUsuario+"!");
+        }
+    }
+}
